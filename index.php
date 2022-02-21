029e4fc4df0fa7fbd3258403e083768bde1b6813
GerardFritzC <100100532+GerardFritzC@users.noreply.github.com>
2022-02-21 17:11:55 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Home Page</title>
+  </head>
+  <body>
+    <h1>Welcome to My Website! I am Gerard Fritz Chuatico!</h1>
+  <ul>
+    <li><a href="index.php">HOME</a></li>
+    <li><a hre="/LT2/Learnin task 2.html">LEARNING TASK 2</a></li>
+  </ul>
+  </body
+</html>
